cbd5f4253a5d5df3883c54396e65c0206d8709ac
amzrk2 <amzrk2@outlook.com>
2020-07-13 00:44:38 +0800

    Switch to vue-analytics

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import VueAnalytics from 'vue-analytics';
+Vue.use(VueAnalytics, {
+  id: 'UA-146011233-7',
+  router,
+});
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
