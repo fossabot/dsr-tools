4cf8cad34ce8e566268623cf0f96267ed63d9c10
amzrk2 <amzrk2@outlook.com>
2020-08-09 10:48:18 +0800

    update: update code format

--- src/assets/changelog.js
+++ src/assets/changelog.js
@@ -2,21 +2,25 @@ export default [
   {
     version: 'v3.0.3',
     date: '2020-07-27',
-    logs: ['修改路由滚动行为', '移除对 jsDelivr API 的使用', '修复 TempAlert 组件 sessionStorage 使用问题'],
+    logs: [
+      '修改路由滚动行为',
+      '移除对 jsDelivr API 的使用',
+      '修复 TempAlert 组件 sessionStorage 使用问题'
+    ],
     active: true,
-    latest: true,
+    latest: true
   },
   {
     version: 'v3.0.2',
     date: '2020-07-19',
     logs: ['默认强制拉取最新版本数据规避缓存影响'],
-    active: false,
+    active: false
   },
   {
     version: 'v3.0.1-hotfix',
     date: '2020-07-14',
     logs: ['修复缓存时间记录错误的问题'],
-    active: false,
+    active: false
   },
   {
     version: 'v3.0.1',
@@ -27,9 +31,9 @@ export default [
       '修复 Google Analytics',
       '添加 GitHub Actions 测试',
       '修复 SEO Tags',
-      '修复关于页面列表样式',
+      '修复关于页面列表样式'
     ],
-    active: false,
+    active: false
   },
   {
     version: 'v3.0.0',
@@ -41,74 +45,79 @@ export default [
       'FFXIV 国服狩猎车时间表模块更新',
       'Minecraft DSRCA 整合包发布模块更新',
       '关于页面更新',
-      'Crisp 客服支持',
+      'Crisp 客服支持'
     ],
-    active: false,
+    active: false
   },
   {
     version: 'v2.3.1',
     date: '2020-05-14',
     logs: ['狩猎车页面添加导航锚链接', '狩猎车页面添加返回顶部', '修复部分样式错误'],
-    active: false,
+    active: false
   },
   {
     version: 'v2.3.0',
     date: '2020-03-28',
-    logs: ['考虑稳定性原因对服务器进行迁移', '全站样式更新', '狩猎车添加 4.0 车数据', '移除关于页面'],
-    active: false,
+    logs: [
+      '考虑稳定性原因对服务器进行迁移',
+      '全站样式更新',
+      '狩猎车添加 4.0 车数据',
+      '移除关于页面'
+    ],
+    active: false
   },
   {
     version: 'v2.2.0',
     date: '2020-03-02',
     logs: ['二级页面迁移', '首页背景图片更新'],
-    active: false,
+    active: false
   },
   {
     version: 'v2.1.1',
     date: '2020-02-14',
     logs: ['修改狩猎车时间表问题提交链接'],
-    active: false,
+    active: false
   },
   {
     version: 'v2.1.0',
     date: '2020-02-10',
     logs: ['添加 FFXIV 国服狩猎车时间表模块'],
-    active: false,
+    active: false
   },
   {
     version: 'v2.0.0',
     date: '2020-01-14',
     logs: ['Bootstrap v4 重构站点'],
-    active: false,
+    active: false
   },
   {
     version: 'v1.3.0',
     date: '2019-12-30',
     logs: ['添加关于页面'],
-    active: false,
+    active: false
   },
   {
     version: 'v1.2.0',
     date: '2019-12-25',
     logs: ['添加截图存档模块', '主页背景图片更新'],
-    active: false,
+    active: false
   },
   {
     version: 'v1.1.1',
     date: '2019-10-12',
     logs: ['添加静态资源 CDN 加速', '修复部分页面问题'],
-    active: false,
+    active: false
   },
   {
     version: 'v1.1.0',
     date: '2019-08-30',
     logs: ['添加 Minecraft DSRCA 整合包发布模块'],
-    active: false,
+    active: false
   },
   {
     version: 'v1.0.0',
     date: '2019-08-21',
     logs: ['站点首页上线'],
-    active: false,
-  },
+    active: false
+  }
 ];
