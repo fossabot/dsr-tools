b06f459dda9eb03629bcb6fd07f43190207c4c2b
amzrk2 <amzrk2@outlook.com>
2020-10-09 17:27:54 +0800

    update: api proxy script

--- src/plugins/bangumi/apiProxy.js
+++ src/plugins/bangumi/apiProxy.js
@@ -1,77 +1,72 @@
 const API_URL = 'https://api.bgm.tv/';
-const ALLOW_REFERER = [
+const API_PATH = /^\/bgm(.*)/;
+const ALLOWED_ORIGIN = [
   /^https?:\/\/dsrca\.amzrk2\.cc/,
   /^https?:\/\/api-bgm\.amzrk2\.workers\.dev/,
-  /^https?:\/\/localhost/
+  /^https?:\/\/localhost/,
 ];
-const API_PATH = /^\/bgm(.*)/;
 
 /**
- * Validate referer
- * @param {String} referer
+ * 验证 Origin 头
+ * @param {String} origin
  */
-function validateReferer(referer) {
-  console.log(referer);
-  // If CORS
-  if (referer) {
-    for (let i = 0; i < ALLOW_REFERER.length; i++) {
-      if (ALLOW_REFERER[i].exec(referer)) {
-        return true;
+function validateOrigin(origin) {
+  if (origin && origin.includes('https')) {
+    for (let i = 0; i < ALLOWED_ORIGIN.length; i++) {
+      if (ALLOWED_ORIGIN[i].exec(origin)) {
+        return true; // 通过
       }
     }
   }
-  return false;
+  return false; // 拒绝所有非 CORS 请求和非 SSL 请求
 }
 
 /**
- * Validate pathname
+ * 解析 pathname
  * @param {String} url
  */
 function validatePath(pathname) {
   const parsedPath = API_PATH.exec(pathname);
   if (parsedPath && parsedPath.length > 1) {
     if (parsedPath[1] === '') {
-      return '/';
+      return '/'; // 这种情况是为了代理根目录 `api.bgm.tv/` => `worker.example.org/bgm/`
     } else {
-      return parsedPath[1];
+      return parsedPath[1]; // `api.bgm.tv/*` => `worker.example.org/bgm/*`
     }
   } else {
-    return false;
+    return false; // 若不是对 `/bgm*` 的访问则拒绝
   }
 }
 
 /**
- * Proxy API
- * @param {Request} request
- * @param {String} pathname
+ * 添加 CORS 头
+ * @param {Request} request - 源请求
+ * @param {String} origin - 源 origin
+ * @param {String} pathname - 源请求解析出的正确 pathname
  */
-async function handleRequest(request, pathname) {
-  const apiUrl = new URL(API_URL);
-  apiUrl.pathname = pathname;
-  // Rewrite request to point to API url which also makes the request mutable
-  // so we can add the correct Origin header to make the API server think
-  // that this request isn't cross-site
-  request = new Request(apiUrl, request);
-  request.headers.set('Origin', apiUrl.origin);
-  let response = await fetch(request);
-  // Recreate the response so we can modify the headers
-  response = new Response(response.body, response);
-  // Set CORS headers
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  // Append to/Add Vary header so browser will cache response correctly
-  response.headers.append('Vary', 'Origin');
+async function handleRequest(request, origin, pathname) {
+  const apiUrl = new URL(API_URL); // 构建一个新的 URL 对象 `api.bgm.tv/`
+  apiUrl.pathname = pathname; // 设置正确的 pathname
+  request = new Request(apiUrl, request); // 覆盖源 request
+  request.headers.set('Origin', apiUrl.origin); // 伪装 Origin
+  let response = await fetch(request); // 获取响应
+  response = new Response(response.body, response); // 覆盖响应 response
+  response.headers.set('Access-Control-Allow-Origin', origin); // 设置 CORS 头
+  response.headers.append('Vary', 'Origin'); // 设置 Vary 头使浏览器正确进行缓存
   return response;
 }
 
-addEventListener('fetch', event => {
-  // Get request data
+addEventListener('fetch', (event) => {
+  // 获取请求的信息
   const request = event.request;
   const url = new URL(request.url);
-  // Validate
-  const validReferer = validateReferer(request.headers.get('Referer'));
-  const validPath = validatePath(url.pathname);
-  if (validReferer && validPath) {
-    event.respondWith(handleRequest(request, validPath));
+  const origin = request.headers.get('Origin');
+  const pathname = url.pathname;
+  // 验证和解析
+  const validOrigin = validateOrigin(origin);
+  const validPath = validatePath(pathname);
+  if (validOrigin && validPath) {
+    event.respondWith(handleRequest(request, origin, validPath));
   } else {
     const response = new Response('[BGM API Proxy] Request not allowed', { status: 403 });
     event.respondWith(response);
