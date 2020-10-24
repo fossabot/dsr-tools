734ac20665297535a4eb50bf58e99042db07bc26
amzrk2 <amzrk2@outlook.com>
2020-10-24 15:33:25 +0800

    update: remove analytics

--- src/main.js
+++ src/main.js
@@ -6,17 +6,9 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-import VueAnalytics from 'vue-analytics';
-if (process.env.NODE_ENV === 'production') {
-  Vue.use(VueAnalytics, {
-    id: 'UA-146011233-7',
-    router,
-  });
-}
-
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app');
